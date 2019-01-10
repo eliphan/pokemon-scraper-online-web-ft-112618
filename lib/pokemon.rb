efdf29568408254f4e78c4f9add61d0a80a4bcc5
Elise Phan <aliceconnic@gmail.com>
2019-01-10 21:42:18 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,5 +2,11 @@ class Pokemon
   attr_accessor :id,:name,:type,:db,:hp
   
   def initialize(id:,name:,type:,db:,hp:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+    @hp = hp
+  end
   
 end
